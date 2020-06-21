cb310bd4c54230ba3689801cf5524ae7ae690051
piyawatd <arunwatd@gmail.com>
2020-06-21 21:46:57 +0700

    add checkusername and password pattern

--- public/js/dataservice.js
+++ public/js/dataservice.js
@@ -7,6 +7,16 @@ function replacetext(replacetext)
     return newtext;
 }
 
+function checkusername(value) {
+    var regex = /^[A-Za-z\d_]{5,}$/;
+    return regex.test(value);
+}
+
+function checkpassword(value) {
+    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{5,}$/;
+    return regex.test(value);
+}
+
 function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
