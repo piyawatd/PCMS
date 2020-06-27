bb194d4347b71fdc08b4829c7d3ecd1270935b2c
piyawatd <arunwatd@gmail.com>
2020-06-27 21:19:54 +0700

    fix js css

--- resources/views/Admins/admin/table.blade.php
+++ resources/views/Admins/admin/table.blade.php
@@ -12,14 +12,14 @@
   <title>SB Admin 2 - Tables</title>
 
   <!-- Custom fonts for this template -->
-  <link href="{{asset('/assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="{{asset('/public/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template -->
-  <link href="{{asset('/assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/public/css/sb-admin-2.css')}}" rel="stylesheet">
 
   <!-- Custom styles for this page -->
-  <link href="{{asset('/assets/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+  <link href="{{asset('/public/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
 
 </head>
 
@@ -587,21 +587,21 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="{{asset('/assets/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('/public/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('/public/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="{{asset('/assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+  <script src="{{asset('/public/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="{{asset('/assets/js/sb-admin-2.min.js')}}"></script>
+  <script src="{{asset('/public/js/sb-admin-2.min.js')}}"></script>
 
   <!-- Page level plugins -->
-  <script src="{{asset('/assets/vendor/datatables/jquery.dataTables.min.js')}}"></script>
-  <script src="{{asset('/assets/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+  <script src="{{asset('/public/vendor/datatables/jquery.dataTables.min.js')}}"></script>
+  <script src="{{asset('/public/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
 
   <!-- Page level custom scripts -->
-  <script src="{{asset('/assets/js/demo/datatables-demo.js')}}"></script>
+  <script src="{{asset('/public/js/demo/datatables-demo.js')}}"></script>
 
 </body>
 
