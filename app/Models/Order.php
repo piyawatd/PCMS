5c82213742320b324fe783714e257e439631fead
piyawatd <arunwatd@gmail.com>
2020-07-18 11:10:20 +0700

    add function for get order data

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+
+    //default value
+    protected $attributes = [
+        'status' => true,
+    ];
+
+    public function getCustomerDataAttribute()
+    {
+        return Customer::find($this->customer);
+    }
+
+    public function getItemsAttribute()
+    {
+        return OrderItem::where("order",$this->id)->get();
+    }
+
+    public function getShippingAttribute()
+    {
+
+        return OrderAddress::where("order",$this->id)->Where("type","shipping")->get();
+    }
+
+    public function getBillingAttribute()
+    {
+        return OrderAddress::where("order",$this->id)->Where("type","billing")->get();
+    }
 }
